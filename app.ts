85de873d82ee61082f1ee9f619128176a140da9f
pluto-atom-4 <genko2@hotmail.com>
2019-09-19 12:28:52 -0700

    added various functions

--- app.ts
+++ app.ts
@@ -41,3 +41,29 @@ let  myCar : any = "BMW";
 console.log(myCar);
 myCar = {Brand: "BMW", Series: 3};
 console.log(myCar);
+
+// functions
+function returnMyName(): string {
+  return myName;
+}
+console.log(returnMyName());
+
+// void
+function sayHello(): void {
+  console.log("Hello!");
+}
+
+// argument types
+function multiply(value1: number, value2: number): number {
+  return value1 * value2;
+}
+//console.log(multiply(2, 'Max'))
+console.log(multiply(10, 2));
+
+// function types
+let myMultiply: (val1: number, val2: number) => number;
+
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(5,2));
