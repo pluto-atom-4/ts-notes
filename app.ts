79e290c33406a3289eef57773fb57d077ef5e086
pluto-atom-4 <genko2@hotmail.com>
2019-09-29 15:00:25 -0700

    defined interface with properties
    - nullable
    - dynamic

--- app.ts
+++ app.ts
@@ -1,129 +1,22 @@
-// string
-let myName = 'Max';
-// myName = 28;
-
-// number
-let myAge: number = 27.5;
-//myAge = 'Max';
-
-
-// boolean
-let hasHobbies: boolean = false;
-// hasHobbies = 1;
-
-// assign types
-let myRealAge: number;
-myRealAge = 27;
-// myRealAge = '27';
-
-
-// array
-let hobbies: any[] = ["Cooking", "Sports"];
-hobbies = [100];
-// hobbies = 100;
-
-
-// tuples
-let address: [string, number] = ["Superstreet", 99];
-
-// enum
-enum Color {
-  Grey,
-  Green = 100,
-  Blue = 2
+interface NamedPerson {
+  firstName: string;
+  age?: number;
+  [propName: string]: any;
 }
 
-let myColor = Color.Green;
-console.log(myColor);
-
-
-let  myCar : any = "BMW";
-console.log(myCar);
-myCar = {Brand: "BMW", Series: 3};
-console.log(myCar);
-
-// functions
-function returnMyName(): string {
-  return myName;
-}
-console.log(returnMyName());
-
-// void
-function sayHello(): void {
-  console.log("Hello!");
+function greet(person: NamedPerson) {
+  console.log("Hello " + person.firstName);
 }
 
-// argument types
-function multiply(value1: number, value2: number): number {
-  return value1 * value2;
+function changeName(person: NamedPerson) {
+  person.firstName = "Anna";
 }
-//console.log(multiply(2, 'Max'))
-console.log(multiply(10, 2));
 
-// function types
-let myMultiply: (val1: number, val2: number) => number;
-
-// myMultiply = sayHello;
-// myMultiply();
-myMultiply = multiply;
-console.log(myMultiply(5,2));
-
-// objects
-//            type definition                 real object
-let userData: { name: string, age: number } = {
-  name: "Max",
-  age: 27
-};
-// userData = {
-//   a: "Hello",
-//   b: 22
-// };
-
-// complex object
-//  var name   type definition
-//               array of number function
-let complex: { data: number[], output: (all: boolean) => number[]} = {
-  data: [100, 3.99, 10],
-
-  output: function (all: boolean): number[] {
-    return this.data;
-  }
-};
-// invalid..
-// complex = {};
-
-// how to reuse the type definition ? => alias is the one
-type Complex = { data: number[], output: (all: boolean) => number[]};
-
-let complex2: Complex = {
-  data: [100, 3.99, 10],
-
-  output: function (all: boolean): number[] {
-    return this.data;
-  }
+const person = {
+  firstName: "Max",
+  hobbies: ["Cooking", "Sports"]
 };
 
-// union types
-let myRealRealAge: number | string = 27;
-myRealRealAge = "27";
-// myRealRealAge = true;
-
-// check types
-let finalValue = 27;
-if (typeof finalValue == "number") {
-  console.log("Final value is a number")
-}
-
-// never type
-function neverReturns(): never {
-  throw  new Error('An error!');
-}
-
-// non nullable type
-let canBeNull: number | null = 12;
-canBeNull = null;
-let canBeAlsoNull;
-canBeAlsoNull = null;
-
-let canThisBeAny: number | null = null;
-canThisBeAny = 12;
+greet({firstName: "Max", age: 27});
+changeName(person);
+greet(person);
